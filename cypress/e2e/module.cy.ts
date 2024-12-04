a51c9c3d5bf846f290373371d8bc1c08cf5d8f07
markuczy <martin.kuczynski@capgemini.com>
2024-12-04 17:01:51 +0100

    cypress setup enchance

---
+++ cypress/e2e/module.cy.ts
@@ -0,0 +1,16 @@
+describe('module spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  // TODO: Make the url as parameter
+  // TODO: Offer login functionality for products out of the box
+  // TODO: Write real assertion to check for the module loading
+  it('renders the page', () => {
+    cy.visit('http://local-proxy/onecx-shell/admin/tenant')
+
+    cy.origin('http://keycloak-app', () => {
+      cy.title().should('eq', 'Sign in to')
+    })
+  })
+})
--- e2e/module.cy.js
+++
@@ -1,8 +0,0 @@
-describe('module spec', () => {
-  it('renders the page', () => {
-    cy.visit('http://local-proxy/onecx-shell/admin/tenant')
-
-    cy.title().should('eq', 'My awesome title')
-  })
-})
-// Sign in to onecx
